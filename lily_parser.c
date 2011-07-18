0aef2eef7de089e4e86695fb7c4f5d6380920b23
Jesse Ray Adkins <jesserayadkins@gmail.com>
2011-07-17 17:36:35 -0700

    Create a basic token, and share it between fe lexer+parser.
    Hopefully, there will only be a need for one token at a time. If so, then only
    one word buffer will be needed.
    
    Created ch_class to give 'classes' to different types of scanned data.

--- lily_parser.c
+++ lily_parser.c
@@ -1,4 +1,12 @@
+#include "lily_lexer.h"
 #include "lily_parser.h"
+
+static lily_token *tok;
+
+static void next_token()
+{
+    
+}
 
 void lily_init_parser(lily_parser_data *d)
 {
@@ -7,5 +15,5 @@ void lily_init_parser(lily_parser_data *d)
 
 void lily_parser(void)
 {
-
+    tok = lily_lexer_token();
 }
