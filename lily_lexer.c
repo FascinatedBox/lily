b41d5ec26409ec77539714e4b368383e40cd4168
Fascinated Box <FascinatedBox@gmail.com>
2011-07-17 17:36:35 -0700

    Create a basic token, and share it between fe lexer+parser.
    Hopefully, there will only be a need for one token at a time. If so, then only
    one word buffer will be needed.
    
    Created ch_class to give 'classes' to different types of scanned data.

--- lily_lexer.c
+++ lily_lexer.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "lily_lexer.h"
 #include "lily_impl.h"
@@ -9,6 +10,11 @@ static FILE *lex_file;
 static char *lex_buffer;
 static int lex_bufsize;
 static int lex_bufpos;
+static lily_token *lex_token;
+
+static char ch_class[255];
+#define CH_INVALID  0
+#define CH_WORD     1
 
 void lily_init_lexer(char *filename)
 {
@@ -19,6 +25,15 @@ void lily_init_lexer(char *filename)
     lex_buffer = malloc(1024 * sizeof(char));
     if (lex_buffer == NULL)
         lily_impl_fatal("No memory to init lexer.\n");
+
+    lex_token = malloc(sizeof(lily_token));
+    if (lex_token == NULL)
+        lily_impl_fatal("No memory to create lexer token.\n");
+
+    lex_token->word_buffer = malloc(1024 * sizeof(char));
+    if (lex_token->word_buffer == NULL)
+        lily_impl_fatal("No memory to create lexer token buffer.\n");
+
     lex_bufsize = 1023;
 
     lily_page_data *d = malloc(sizeof(lily_page_data));
@@ -32,4 +47,30 @@ void lily_init_lexer(char *filename)
 
     lily_init_page_scanner(d);
     free(d);
+
+    /* Initialize ch_class, which is used to determine what 'class' a letter
+       is in. */
+    memset(ch_class, CH_INVALID, sizeof(ch_class));
+
+    int i;
+    for (i = 'a';i < 'z';i++)
+        ch_class[i] = CH_WORD;
+
+    for (i = 'A';i < 'Z';i++)
+        ch_class[i] = CH_WORD;
+
+    ch_class[(int)'_'] = CH_WORD;
+
+    /* Make sure the lexer starts after the <@lily block. */
+    lily_page_scanner();
+}
+
+lily_token *lily_lexer_token()
+{
+    return lex_token;
+}
+
+void lily_lexer(void)
+{
+
 }
--- lily_lexer.h
+++ lily_lexer.h
@@ -7,4 +7,16 @@ typedef struct {
     int *lex_bufpos;
 } lily_lexer_data;
 
+typedef enum {
+    tk_invalid,
+    tk_word
+} lily_tok_type;
+
+typedef struct {
+    lily_tok_type tok_type;
+    char *word_buffer;
+} lily_token;
+
+lily_token *lily_lexer_token();
 void lily_init_lexer(char *);
+void lily_lexer(void);
