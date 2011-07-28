0a4c9e7714f0d432c4f799a524601b6a1a274441
Fascinated Box <FascinatedBox@gmail.com>
2011-07-28 10:05:14 -0700

    The parser finally collects the expression in hello_world.ly.
    lily_symbol extended to hold a value. lily_ast extended to hold a var type
    with a symbol value only.
    
    Fixed lily_ast_add_arg not linking the arg up correctly.

--- lily_ast.c
+++ lily_ast.c
@@ -9,7 +9,16 @@ lily_ast *lily_ast_init_call(lily_symbol *s)
 
     a->expr_type = func_call;
     a->data.call.sym = s;
+    a->data.call.num_args = 0;
     a->data.call.args = NULL;
+}
+
+lily_ast *lily_ast_init_var(lily_symbol *s)
+{
+    lily_ast *a = lily_impl_malloc(sizeof(lily_ast));
+
+    a->expr_type = var;
+    a->data.value = s;
 }
 
 void lily_ast_add_arg(lily_ast *func, lily_ast *tree)
@@ -18,5 +27,6 @@ void lily_ast_add_arg(lily_ast *func, lily_ast *tree)
 
     l->ast = tree;
     l->next = func->data.call.args;
-    func->data.call.args = l->next;
+    func->data.call.num_args++;
+    func->data.call.args = l;
 }
