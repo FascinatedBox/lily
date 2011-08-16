17ef9d6ec6f782a3f292a62d0d8ae283d2158c7f
Jesse Ray Adkins <jesserayadkins@gmail.com>
2011-08-16 12:37:03 -0700

    Added binary ast type, basic tree merging, fixed ast_init funcs.
    A step in the right direction for properly working expressions. Note that
    no precedence for binary ops exists yet. lily_ast.c init functions fixed for
    not returning a value at all!
    
    It works...barely.

--- lily_ast.c
+++ lily_ast.c
@@ -1,6 +1,20 @@
 #include "lily_ast.h"
 #include "lily_impl.h"
 
+static lily_expr_op opcode_for_token(lily_tok_type t)
+{
+    lily_expr_op op;
+    switch (t) {
+        case tk_equal:
+            op = expr_assign;
+            break;
+        default:
+            lily_impl_fatal("Invalid token for opcode: %d.\n", t);
+    };
+
+    return op;
+}
+
 lily_ast *lily_ast_init_call(lily_symbol *s)
 {
     lily_ast *a = lily_impl_malloc(sizeof(lily_ast));
@@ -9,6 +23,7 @@ lily_ast *lily_ast_init_call(lily_symbol *s)
     a->data.call.sym = s;
     a->data.call.num_args = 0;
     a->data.call.args = NULL;
+    return a;
 }
 
 lily_ast *lily_ast_init_var(lily_symbol *s)
@@ -17,6 +32,35 @@ lily_ast *lily_ast_init_var(lily_symbol *s)
 
     a->expr_type = var;
     a->data.value = s;
+    return a;
+}
+
+lily_ast *lily_ast_init_binary_op(lily_tok_type t)
+{
+    lily_ast *a = lily_impl_malloc(sizeof(lily_ast));
+
+    a->expr_type = binary;
+    a->data.bin_expr.op = opcode_for_token(t);
+    a->data.bin_expr.left = NULL;
+    a->data.bin_expr.right = NULL;
+    a->data.bin_expr.parent = NULL;
+    return a;
+}
+
+lily_ast *lily_ast_merge_trees(lily_ast *current, lily_ast *new_ast)
+{
+    lily_ast *ret;
+
+    if (current->expr_type == var ||
+        current->expr_type == func_call) {
+        /* current = value, new = binary op. Binary op becomes parent. */
+        new_ast->data.bin_expr.left = current;
+        ret = new_ast;
+    }
+    else
+        lily_impl_fatal("lily_ast_merge_trees: Handle case of current=tree.\n");
+
+    return ret;
 }
 
 void lily_ast_add_arg(lily_ast *func, lily_ast *tree)
--- lily_ast.h
+++ lily_ast.h
@@ -1,14 +1,19 @@
 #ifndef LILY_AST_H
 # define LILY_AST_H
 
+#include "lily_lexer.h"
 #include "lily_symtab.h"
 
 /* Based off of http://lambda.uta.edu/cse5317/notes/node25.html. */
 struct lily_ast_list;
 
+typedef enum {
+    expr_assign,
+} lily_expr_op;
+
 typedef struct lily_ast_ {
     enum {
-        func_call, var
+        func_call, var, binary
     } expr_type;
     int reg_pos;
     union {
@@ -18,6 +23,12 @@ typedef struct lily_ast_ {
             int num_args;
             struct lily_ast_list *args;
         } call;
+        struct {
+            lily_expr_op op;
+            struct lily_ast_ *left;
+            struct lily_ast_ *right;
+            struct lily_ast_ *parent;
+        } bin_expr;
     } data;
 } lily_ast;
 
@@ -28,6 +39,8 @@ struct lily_ast_list {
 
 lily_ast *lily_ast_init_call(lily_symbol *);
 lily_ast *lily_ast_init_var(lily_symbol *);
+lily_ast *lily_ast_init_binary_op(lily_tok_type);
+lily_ast *lily_ast_merge_trees(lily_ast *, lily_ast *);
 void lily_ast_add_arg(lily_ast *, lily_ast *);
 
 #endif
