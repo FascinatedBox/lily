a93c39b8087588c951a0a0706dc8a187c79daccb
Jesse Ray Adkins <jesserayadkins@gmail.com>
2011-09-21 14:06:08 -0700

    Explain the arguments for opcodes in lily_opcode.h

--- lily_opcode.h
+++ lily_opcode.h
@@ -3,9 +3,18 @@
 
 typedef enum {
     o_load_reg,
+    /* Arguments: pos, v
+       Loads the symbol 'v' into the vm register at 'pos'. */
     o_builtin_print,
+    /* Arguments: pos
+       Calls lily_impl_print, using the symbol at vm register 'pos'. */
     o_assign,
+    /* Arguments: left_pos, right_pos
+       Assigns the value in 'right_pos' to 'left_pos'. 'left_pos' and
+       'right_pos' both refer to vm registers. */
     o_vm_return
+    /* Arguments: none
+       This returns control from the lily vm to the parser. */
 } lily_opcode;
 
 #endif
