a8514030651d10095e02ee0e9c3fdb671f90b0a7
Fascinated Box <FascinatedBox@gmail.com>
2012-01-23 19:41:01 -0800

    Objects can now be collected. Added an object decl test in basic_types.ly.
    Note: Assignment is not supported yet, just collection.

--- basic_types.ly
+++ basic_types.ly
@@ -7,6 +7,7 @@
 str a = "a"
 integer b = 1
 number c = 1.0
+object o
 
 # Some extra tests...
 print("Hello")
--- lily_symtab.h
+++ lily_symtab.h
@@ -128,7 +128,7 @@ typedef struct {
 #define SYM_CLASS_FUNCTION 3
 #define SYM_CLASS_OBJECT   4
 
-#define SYM_LAST_CLASS     3
+#define SYM_LAST_CLASS     4
 
 lily_class *lily_class_by_id(lily_symtab *, int);
 lily_class *lily_class_by_name(lily_symtab *, char *);
