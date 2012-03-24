d323760c439d88219094d9b489b0fae7a83d3427
Fascinated Box <FascinatedBox@gmail.com>
2012-03-23 17:58:44 -0700

    Multi-line conditions work! Uncommented the last branch_test.ly test.
    Added 'types' to branches, to store the state of branches. This was needed for
    multi-line conditions. Single-line ifs use the stack as well, but that might
    be changed later.
    
    But more importantly, conditions work! Hurray!

--- lily_emitter.h
+++ lily_emitter.h
@@ -7,10 +7,13 @@
 typedef struct {
     int *patches;
     int *saved_spots;
+    int *types;
     int patch_pos;
     int patch_size;
     int save_pos;
     int save_size;
+    int type_pos;
+    int type_size;
 } lily_branches;
 
 typedef struct {
@@ -24,7 +27,7 @@ typedef struct {
 void lily_emit_ast(lily_emit_state *, lily_ast *);
 void lily_emit_conditional(lily_emit_state *, lily_ast *);
 void lily_emit_new_if(lily_emit_state *);
-void lily_emit_branch_change(lily_emit_state *);
+void lily_emit_branch_change(lily_emit_state *, int);
 void lily_emit_fix_exit_jumps(lily_emit_state *);
 void lily_emit_set_target(lily_emit_state *, lily_var *);
 void lily_emit_vm_return(lily_emit_state *);
