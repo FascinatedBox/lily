7a4e75b52edf73e0d93c7a89593e9529148622b8
Jesse Ray Adkins <jesserayadkins@gmail.com>
2012-05-13 00:01:03 -0700

    Add a block for returns when entering a function. stub return emitting.
    lily_emit_return will eventually verify the type of the return, but currently
    does nothing.

--- lily_emitter.h
+++ lily_emitter.h
@@ -39,6 +39,7 @@ void lily_emit_clear_block(lily_emit_state *, int);
 void lily_emit_push_block(lily_emit_state *, int);
 void lily_emit_pop_block(lily_emit_state *);
 void lily_emit_set_target(lily_emit_state *, lily_var *);
+void lily_emit_return(lily_emit_state *, lily_ast *, lily_sig *);
 void lily_emit_vm_return(lily_emit_state *);
 void lily_free_emit_state(lily_emit_state *);
 lily_emit_state *lily_new_emit_state(lily_excep_data *);
