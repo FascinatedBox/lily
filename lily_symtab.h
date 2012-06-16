4aecd2a0e3709d909ed81642e4d4c1c2ac63d78f
Fascinated Box <FascinatedBox@gmail.com>
2012-06-15 20:50:36 -0700

    Fix lily_try_new_method_val prototype. Add to lily_symtab.h. Oops.

--- lily_symtab.h
+++ lily_symtab.h
@@ -145,5 +145,6 @@ lily_symtab *lily_new_symtab(lily_excep_data *);
 lily_var *lily_new_var(lily_symtab *, lily_class *, char *);
 lily_var *lily_var_by_name(lily_symtab *, char *);
 int lily_try_add_storage(lily_symtab *, lily_class *);
+lily_method_val *lily_try_new_method_val(lily_symtab *);
 void lily_drop_block_vars(lily_symtab *, lily_var *);
 #endif
