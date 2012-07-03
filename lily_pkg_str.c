22630ca2a5d0b8a78b2da7753bda22e770b069f6
Jesse Ray Adkins <jesserayadkins@gmail.com>
2012-07-03 13:26:54 -0700

    str pkg: Fix concat's signature. It takes two strings, and returns one.

--- lily_pkg_str.c
+++ lily_pkg_str.c
@@ -5,7 +5,7 @@ void lily_str_concat(lily_sym **args)
 
 }
 
-static lily_func_seed concat = {"concat", 1, lily_str_concat,
-        {SYM_CLASS_STR, SYM_CLASS_STR}};
+static lily_func_seed concat = {"concat", 2, lily_str_concat,
+        {SYM_CLASS_STR, SYM_CLASS_STR, SYM_CLASS_STR}};
 
 lily_func_seed *str_seeds[] = {&concat};
