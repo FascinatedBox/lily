396490b1d269c8d8960afc1cb47b71bd41e3d8b9
Fascinated Box <FascinatedBox@gmail.com>
2012-07-15 11:58:52 -0700

    The concat function works...sort of.
    It could probably use a more efficient algorithm though, along with a bunch of
    other stuff.
    
    The vm function call op was fixed so that the return would be arg[0], meaning
    the first arg is at [1], the second at [2], and so on. It seems easier to do
    that than to put the arg at the end (first at [0], second at [1]...).
    
    str storages are set to a NULL value on creation, since concat checks for
    previous ones and lily_free_symtab will want to make sure all the strvals
    allocated are deleted. There might be memory leaks resulting from this, but
    there's no gc yet to fix that.

--- lily_pkg_str.c
+++ lily_pkg_str.c
@@ -1,8 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "lily_impl.h"
 #include "lily_pkg.h"
 
 void lily_str_concat(lily_sym **args)
 {
+    lily_strval *ret, *arg1, *arg2;
+    ret = ((lily_strval *)args[0]->value.ptr);
+    arg1 = ((lily_strval *)args[1]->value.ptr);
+    arg2 = ((lily_strval *)args[2]->value.ptr);
 
+    int newsize = arg1->size + arg2->size + 1;
+    /* It's null if [0] is a storage that's never been assigned to before. */
+    if (ret == NULL) {
+        ret = lily_malloc(sizeof(lily_strval));
+        if (ret == NULL)
+            return;
+        ret->str = lily_malloc(sizeof(char) * newsize);
+    }
+    else if (ret->size < newsize) {
+        char *newstr;
+        newstr = realloc(ret->str, sizeof(char) * newsize);
+        if (newstr == NULL) {
+            lily_free(ret->str);
+            lily_free(ret);
+            return;
+        }
+        ret->str = newstr;
+    }
+
+    strcpy(ret->str, arg1->str);
+    strcat(ret->str, arg2->str);
+
+    ret->size = newsize;
+    args[0]->value.ptr = ret;
 }
 
 static lily_func_seed concat = {"concat", 2, lily_str_concat,
