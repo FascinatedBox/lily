9f30d1b699457ea3192e427a479ce82f7493f0d4
Jesse Ray Adkins <jesserayadkins@gmail.com>
2012-10-02 14:27:02 -0700

    Milestone 1: Implement o_save, o_restore. Test added to save_test.ly.
    Added a save stack to vm to get this to work. It's a bit rough, but what's
    important is that save and restore now finally, finally work. Fibonacci is
    implemented a bit strange, because of a couple quirks that I'll fix pretty
    soon.
    
    This feels like a huge accomplishment, and the start of something awesome.

--- lily_vm.h
+++ lily_vm.h
@@ -5,11 +5,20 @@
 # include "lily_symtab.h"
 
 typedef struct {
+    lily_value value;
+    int flags;
+    lily_sym *sym;
+} lily_saved_val;
+
+typedef struct {
     int **saved_code;
     int *saved_pos;
     lily_sym **saved_ret;
+    lily_saved_val *saved_values;
     int stack_pos;
     int stack_size;
+    int val_pos;
+    int val_size;
     lily_excep_data *error;
     lily_var *main;
 } lily_vm_state;
