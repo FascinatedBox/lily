94923b3a168e2f946066ca70719c4f9832fe2126
Jesse Ray Adkins <jesserayadkins@gmail.com>
2012-10-31 10:27:14 -0700

    Introduce lily_aft, the allocation fail test.
    This runs code like lily_fs does (from a .ly file), but has one important
    difference: It allows a set number of allocations to pass through before
    rejecting them. This ensures that a failure to allocate memory at some point
    in Lily won't result in a segfault/invalid read/double free, etc.
    
    On the downside, it requires a double-compilation of all source files. It's
    also rough around the edges still (it only does one pass at a time, but could
    be changed into doing more given some time.

--- lily_impl.h
+++ lily_impl.h
@@ -13,7 +13,16 @@ void lily_impl_debugf(char *, ...);
    to be free'd. */
 void lily_impl_send_html(char *);
 
+#ifndef AFT_ALLOC
 # define lily_malloc(size) malloc(size)
 # define lily_realloc(ptr, size) realloc(ptr, size)
 # define lily_free(ptr) free(ptr)
+#else
+# define lily_malloc(size) aft_malloc(__FILE__, __LINE__, size)
+# define lily_realloc(ptr, size) aft_realloc(__FILE__, __LINE__, ptr, size)
+# define lily_free(ptr) aft_free(__FILE__, __LINE__, ptr)
+extern void *aft_malloc(char *, int, size_t);
+extern void *aft_realloc(char *, int, void *, size_t);
+extern void aft_free(char *, int, void *);
+#endif
 #endif
