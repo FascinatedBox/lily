386ef6410992a80f6b641e815c19a767d558869e
Fascinated Box <FascinatedBox@gmail.com>
2013-06-10 18:52:41 -0700

    lily_strload -> lily_cliexec. It now runs argv[1].
    This was originally done a while back while doing some extensive ast tests. I
    decided to merge it in, because strload was useless as it was. lily_failtest
    verifies that string-based loading works, as well as doing useful tests.
    lily_cliexec is intended to be used for quick code checks without having to
    modify a file.

---
+++ cliexec_main.c
@@ -0,0 +1,74 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lily_parser.h"
+
+/* cliexec_main.c :
+   This starts an interpreter with a string given as a command-line argument.
+   This is a nice tool to quickly check a piece of code, or to toy with the
+   language without editing a file. */
+
+void lily_impl_debugf(char *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    va_end(ap);
+}
+
+void lily_impl_send_html(char *htmldata)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fputs("Usage : lily_cliexec <str>\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    lily_parse_state *parser = lily_new_parse_state(POPT_SHOW_SYMTAB);
+    if (parser == NULL) {
+        fputs("ErrNoMemory: No memory to alloc interpreter.\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (lily_parse_string(parser, argv[1]) == 0) {
+        lily_raiser *raiser = parser->raiser;
+        fprintf(stderr, "%s", lily_name_for_error(raiser->error_code));
+        if (raiser->message)
+            fprintf(stderr, ": %s", raiser->message);
+        else
+            fputc('\n', stderr);
+
+        if (parser->mode == pm_parse) {
+            int line_num;
+            if (raiser->line_adjust == 0)
+                line_num = parser->lex->line_num;
+            else
+                line_num = raiser->line_adjust;
+
+            fprintf(stderr, "Where: File \"%s\" at line %d\n",
+                    parser->lex->filename, line_num);
+        }
+        else if (parser->mode == pm_execute) {
+            lily_vm_stack_entry **vm_stack;
+            lily_vm_stack_entry *entry;
+            int i;
+
+            vm_stack = parser->vm->method_stack;
+            fprintf(stderr, "Traceback:\n");
+            for (i = parser->vm->method_stack_pos-1;i >= 0;i--) {
+                entry = vm_stack[i];
+                fprintf(stderr, "    Method \"%s\" at line %d.\n",
+                        ((lily_var *)entry->method)->name, entry->line_num);
+            }
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    lily_free_parse_state(parser);
+    exit(EXIT_SUCCESS);
+}
