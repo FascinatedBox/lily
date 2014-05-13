7db787eb957c4077b73ccfe11758e2c2f246eb86
Fascinated Box <FascinatedBox@gmail.com>
2014-05-13 12:05:23 -0700

    Remove lily_deref_list_val_by. This is a relic from the circle_buster era.

--- lily_value.h
+++ lily_value.h
@@ -11,7 +11,6 @@ lily_hash_elem *lily_try_new_hash_elem();
 void lily_deref_method_val(lily_method_val *);
 void lily_deref_str_val(lily_str_val *);
 void lily_deref_object_val(lily_object_val *);
-void lily_deref_list_val_by(lily_sig *, lily_list_val *, int);
 void lily_deref_list_val(lily_sig *, lily_list_val *);
 void lily_deref_hash_val(lily_sig *, lily_hash_val *);
 void lily_deref_unknown_val(lily_value *);
