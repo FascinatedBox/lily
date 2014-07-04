14d88e76ddc937125cc907374dfe76132ed979fa
Fascinated Box <FascinatedBox@gmail.com>
2014-07-04 16:01:19 -0700

    Added the beginning of an apache module. Yay!
    Added a rule in configure.ac to build the apache mod only on demand. It's off
    by default in case people don't have the headers to build it but want to build
    the standard lily.
    
    The apache mod has a Makefile.am that replicates most of what's in the
    Makefile generated by `apxs -g -n "lily"' (as far as apache commands from
    within it). This will make it easy to test the apache mod as it's developed.
    
    Note: The apache module does not do anything except to show that it's excited
    that it works when polled. The impl's for printing are stubbed out, and the
    mod doesn't create a working parser.
    
    It's a start!

---
+++ src/apache/mod_lily.c
@@ -0,0 +1,50 @@
+/*  mod_lily.c
+    This is an apache binding for the Lily language. */
+
+#include "httpd.h"
+#include "http_config.h"
+#include "http_protocol.h"
+#include "ap_config.h"
+
+#include "lily_parser.h"
+
+void lily_impl_debugf(char *format, ...)
+{
+    /* todo: The interpreter needs to provide the request_rec. */
+}
+
+void lily_impl_send_html(char *htmldata)
+{
+    /* todo: The interpreter needs to provide the request_rec. */
+}
+
+/* The sample content handler */
+static int lily_handler(request_rec *r)
+{
+    if (strcmp(r->handler, "lily"))
+        return DECLINED;
+
+    r->content_type = "text/html";
+
+    if (!r->header_only)
+        ap_rputs("mod_lily is up and running. Yay!\n", r);
+
+    return OK;
+}
+
+static void lily_register_hooks(apr_pool_t *p)
+{
+    ap_hook_handler(lily_handler, NULL, NULL, APR_HOOK_MIDDLE);
+}
+
+/* Dispatch list for API hooks */
+module AP_MODULE_DECLARE_DATA lily_module = {
+    STANDARD20_MODULE_STUFF,
+    NULL,                  /* create per-dir    config structures */
+    NULL,                  /* merge  per-dir    config structures */
+    NULL,                  /* create per-server config structures */
+    NULL,                  /* merge  per-server config structures */
+    NULL,                  /* table of config file commands       */
+    lily_register_hooks    /* register hooks                      */
+};
+
