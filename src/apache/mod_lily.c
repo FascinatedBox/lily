a6557f22dd2c097287d0c6a8672faee0f0ed1dab
Fascinated Box <FascinatedBox@gmail.com>
2014-07-08 22:33:29 -0700

    apache: Create server package, server::env
    This creates the server package and gives it server::env, which is a
    hash[str, str] containing common environment variables.

--- src/apache/mod_lily.c
+++ src/apache/mod_lily.c
@@ -6,9 +6,12 @@
 #include "http_protocol.h"
 #include "ap_config.h"
 #include "apr_file_io.h"
+#include "util_script.h"
 
 #include "lily_parser.h"
 #include "lily_lexer.h"
+#include "lily_value.h"
+#include "lily_impl.h"
 
 void lily_impl_puts(void *data, char *text)
 {
@@ -140,6 +143,187 @@ static void apache_close_fn(lily_lex_entry *entry)
     apr_file_close(entry->source);
 }
 
+static lily_value *bind_str(lily_sig *str_sig, const char *str)
+{
+    lily_value *new_value = lily_malloc(sizeof(lily_value));
+    lily_str_val *sv = lily_malloc(sizeof(lily_str_val));
+    int str_size = strlen(str);
+    char *buffer = lily_malloc(str_size + 1);
+
+    if (sv == NULL || buffer == NULL || new_value == NULL) {
+        lily_free(sv);
+        lily_free(buffer);
+        lily_free(new_value);
+        return NULL;
+    }
+
+    strcpy(buffer, str);
+    sv->refcount = 1;
+    sv->str = buffer;
+    sv->size = str_size;
+
+    new_value->value.str = sv;
+    new_value->flags = 0;
+    new_value->sig = str_sig;
+
+    return new_value;
+}
+
+static void deref_destroy_value(lily_value *value)
+{
+    if (value != NULL) {
+        lily_deref_unknown_val(value);
+        lily_free(value);
+    }
+}
+
+static lily_hash_elem *bind_hash_elem_with_values(char *sipkey,
+    lily_value *key, lily_value *value)
+{
+    lily_hash_elem *elem = lily_malloc(sizeof(lily_hash_elem));
+
+    if (elem == NULL || key == NULL) {
+        lily_free(elem);
+        return NULL;
+    }
+
+    elem->next = NULL;
+    elem->key_siphash = lily_calculate_siphash(sipkey, key);
+    elem->elem_key = key;
+    elem->elem_value = value;
+    return elem;
+}
+
+struct bind_data {
+    lily_parse_state *parser;
+    request_rec *r;
+    int ok;
+    lily_sig *str_sig;
+    lily_hash_val *hash_val;
+    char *sipkey;
+};
+
+static int bind_env_entry(void *data, const char *key, const char *value)
+{
+    struct bind_data *d = data;
+
+    lily_value *elem_key = bind_str(d->str_sig, key);
+    lily_value *elem_value = bind_str(d->str_sig, value);
+    lily_hash_elem *new_elem = bind_hash_elem_with_values(d->sipkey,
+            elem_key, elem_value);
+
+    if (elem_key == NULL || elem_value == NULL || new_elem == NULL) {
+        lily_free(new_elem);
+        deref_destroy_value(elem_key);
+        deref_destroy_value(elem_value);
+        d->ok = 0;
+        return FALSE;
+    }
+
+    new_elem->next = d->hash_val->elem_chain;
+    d->hash_val->elem_chain = new_elem;
+    return TRUE;
+}
+
+static void bind_env(int *count, lily_parse_state *parser, request_rec *r)
+{
+    if (*count == -1)
+        return;
+
+    lily_symtab *symtab = parser->symtab;
+
+    /* hash[str, str] */
+    const int ids[] = {SYM_CLASS_HASH, SYM_CLASS_STR, SYM_CLASS_STR};
+
+    lily_sig *hash_sig = lily_try_sig_from_ids(symtab, ids);
+    if (hash_sig == NULL) {
+        *count = -1;
+        return;
+    }
+
+    lily_var *env_var = lily_try_new_var(symtab, hash_sig, "env", 0);
+    if (env_var == NULL) {
+        *count = -1;
+        return;
+    }
+
+    lily_hash_val *env_hash = lily_try_new_hash_val();
+    if (env_hash == NULL) {
+        *count = -1;
+        return;
+    }
+
+    env_var->value.hash = env_hash;
+    env_var->flags &= ~VAL_IS_NIL;
+
+    lily_class *str_cls = lily_class_by_id(parser->symtab, SYM_CLASS_STR);
+    struct bind_data data;
+    data.parser = parser;
+    data.r = r;
+    data.ok = 1;
+    data.str_sig = str_cls->sig;
+    data.hash_val = env_hash;
+    data.sipkey = parser->vm->sipkey;
+    apr_table_do(bind_env_entry, &data, r->subprocess_env, NULL);
+
+    if (data.ok == 0)
+        *count = -1;
+    else
+        *count = *count + 1;
+}
+
+static int apache_bind_server(lily_parse_state *parser, request_rec *r)
+{
+    int ret = 1;
+    lily_symtab *symtab = parser->symtab;
+    lily_class *package_cls = lily_class_by_id(symtab, SYM_CLASS_PACKAGE);
+    lily_sig *package_sig = lily_try_sig_for_class(symtab, package_cls);
+    lily_var *bound_var = lily_try_new_var(symtab, package_sig, "server", 0);
+    if (bound_var) {
+        lily_var *save_top = symtab->var_top;
+        int save_spot = symtab->next_register_spot;
+        int count = 0;
+
+        bind_env(&count, parser, r);
+
+        if (count != -1) {
+            lily_package_val *pval = lily_malloc(sizeof(lily_package_val));
+            lily_var **package_vars = lily_malloc(count * sizeof(lily_var *));
+            if (pval == NULL || package_vars == NULL) {
+                lily_free(pval);
+                lily_free(package_vars);
+                ret = 0;
+            }
+            else {
+                int i = 0;
+                lily_var *var_iter = save_top->next;
+                while (var_iter) {
+                    package_vars[i] = var_iter;
+                    i++;
+                    var_iter = var_iter->next;
+                }
+                symtab->var_top = save_top;
+                save_top->next = NULL;
+                symtab->next_register_spot = save_spot;
+
+                pval->refcount = 1;
+                pval->name = bound_var->name;
+                pval->gc_entry = NULL;
+                pval->var_count = i;
+                pval->vars = package_vars;
+                bound_var->flags &= ~VAL_IS_NIL;
+                bound_var->value.package = pval;
+            }
+        }
+        else
+            ret = 0;
+    }
+    else
+        ret = 0;
+
+    return ret;
+}
+
 /* The sample content handler */
 static int lily_handler(request_rec *r)
 {
@@ -159,6 +343,13 @@ static int lily_handler(request_rec *r)
 
     lily_parse_state *parser = lily_new_parse_state(r, 0, NULL);
     if (parser == NULL)
+        return DECLINED;
+
+    /* This populates r->subprocess_env, which is then looped over to make
+       server::env. */
+    ap_add_cgi_vars(r);
+    ap_add_common_vars(r);
+    if (apache_bind_server(parser, r) == 0)
         return DECLINED;
 
     lily_parse_special(parser, lm_tags, lily_file, r->filename,
