FascinatedBox/lily

commit 963b7b9f843291f004a65da1d5d0b7a109c8f341

lily_build_ensure_sig, build non-templated sigs for call returns.
There is a new function called lily_build_ensure_sig. This function takes
the symtab, a lily class, a siglist count, a siglist, and a siglist offset.
From these, it builds a sig and uses the siglist (without mallocing) to do a
lookup. If the sig doesn't exist, then it's created and installed in the
symtab.

This function is used in two ways: The emitter uses it to build a signature
that doesn't have templates from one that does (this is necessary for returns
like list[A] or maybe[A]). The parser now uses this to build signatures for
simple templated types.

This will cut down on memory usage from the parser (no more creating a list
of integer only to have to throw it away). I also unified parsing for lists,
hashes, and tuples to cut down on redundancy.

I also kicked out the circularity + template counting code into a new symtab
function called finalize_sig. Symtab is a bit rougher until finalize sig is
finally gone.

src/lily_symtab.c
@@ -921,6 +921,79 @@ static void get_template_max(lily_sig *sig, int *template_max)
     }
 }
 
+/*  lookup_sig
+    Determine if the current signature exists in the symtab.
+
+    Success: The signature from the symtab is returned.
+    Failure: NULL is returned. */
+lily_sig *lookup_sig(lily_symtab *symtab, lily_sig *input_sig)
+{
+    lily_sig *iter_sig = symtab->root_sig;
+    lily_sig *ret = NULL;
+
+    /* This just means that input_sig was the last signature created. */
+    if (iter_sig == input_sig)
+        iter_sig = iter_sig->next;
+
+    while (iter_sig) {
+        if (iter_sig->cls == input_sig->cls) {
+            if (iter_sig->siglist      != NULL &&
+                iter_sig->siglist_size == input_sig->siglist_size &&
+                iter_sig               != input_sig) {
+                int i, match = 1;
+                for (i = 0;i < iter_sig->siglist_size;i++) {
+                    if (iter_sig->siglist[i] != input_sig->siglist[i]) {
+                        match = 0;
+                        break;
+                    }
+                }
+
+                if (match == 1) {
+                    ret = iter_sig;
+                    break;
+                }
+            }
+        }
+
+        iter_sig = iter_sig->next;
+    }
+
+    return ret;
+}
+
+/*  finalize_sig
+    Determine if the given signature is circular. Also, if its class is not the
+    template class, determine how many templates the signature uses.
+
+    The symtab doesn't use this information at all. These are convenience
+    things for the emitter and the vm. */
+void finalize_sig(lily_sig *input_sig)
+{
+    if (input_sig->siglist) {
+        /* functions are not containers, so circularity doesn't apply to them. */
+        if (input_sig->cls->id != SYM_CLASS_FUNCTION) {
+            int i;
+            for (i = 0;i < input_sig->siglist_size;i++) {
+                if (input_sig->siglist[i]->flags & SIG_MAYBE_CIRCULAR) {
+                    input_sig->flags |= SIG_MAYBE_CIRCULAR;
+                    break;
+                }
+            }
+        }
+
+        /* Find out the highest template index that this type has inside of it.
+           For functions, this allows the emitter to reserve blank sigs for
+           holding template matches. For other sigs, it allows the emitter to
+           determine if a call result uses templates (since it has to be broken
+           down if it does. */
+        if (input_sig->cls->id != SYM_CLASS_TEMPLATE) {
+            int max = 0;
+            get_template_max(input_sig, &max);
+            input_sig->template_pos = max;
+        }
+    }
+}
+
 /* lily_ensure_unique_sig
    This looks through the symtab's current signatures to see if any describe the
    same thing as the given signature.
@@ -972,27 +1045,7 @@ lily_sig *lily_ensure_unique_sig(lily_symtab *symtab, lily_sig *input_sig)
         iter_sig = iter_sig->next;
     }
 
-    /* input_sig can be circular if anything that it holds can be circular.
-       This is provided as a convenience for the vm so it can quickly determine
-       if something should have a gc_entry.
-       function is excluded because it's not a container (the inner types are
-       arguments). */
-    if (input_sig->siglist && input_sig->cls->id != SYM_CLASS_FUNCTION) {
-        int i;
-        for (i = 0;i < input_sig->siglist_size;i++) {
-            if (input_sig->siglist[i]->flags & SIG_MAYBE_CIRCULAR) {
-                input_sig->flags |= SIG_MAYBE_CIRCULAR;
-                break;
-            }
-        }
-    }
-    else if (input_sig->cls->id == SYM_CLASS_FUNCTION) {
-        /* Count the number of templates for the emitter, so that it knows how
-           many signatures to reserve for template matching. */
-        int max = 0;
-        get_template_max(input_sig, &max);
-        input_sig->template_pos = max;
-    }
+    finalize_sig(input_sig);
 
     if (match) {
         /* Remove input_sig from the symtab's sig chain. */
@@ -1017,4 +1070,48 @@ lily_sig *lily_ensure_unique_sig(lily_symtab *symtab, lily_sig *input_sig)
     }
 
     return input_sig;
+}
+
+lily_sig *lily_build_ensure_sig(lily_symtab *symtab, lily_class *cls,
+        int entries_to_use, lily_sig **siglist, int offset)
+{
+    lily_sig fake_sig;
+
+    fake_sig.cls = cls;
+    fake_sig.template_pos = 0;
+    fake_sig.siglist = siglist + offset;
+    fake_sig.siglist_size = entries_to_use;
+    fake_sig.flags = 0;
+    fake_sig.next = NULL;
+
+    fprintf(stderr, "build ensure: there are %d sigs available.\n", entries_to_use);
+    fprintf(stderr, "build ensure: the first one is %p/%s.\n", siglist[offset],
+            siglist[offset]->cls->name);
+    /* The reason it's done like this is purely to save memory. There's no
+       point in creating a new signature if it already exists (since that just
+       means the new one has to be destroyed). */
+    lily_sig *result_sig = lookup_sig(symtab, &fake_sig);
+    if (result_sig == NULL) {
+        lily_sig *new_sig = lily_malloc(sizeof(lily_sig));
+        lily_sig **new_siglist = lily_malloc(entries_to_use *
+                sizeof(lily_sig *));
+
+        if (new_sig == NULL || new_siglist == NULL) {
+            lily_free(new_sig);
+            lily_free(new_siglist);
+            lily_raise_nomem(symtab->raiser);
+        }
+
+        memcpy(new_sig, &fake_sig, sizeof(lily_sig));
+        memcpy(new_siglist, siglist + offset, sizeof(lily_sig *) * entries_to_use);
+        new_sig->siglist = new_siglist;
+
+        new_sig->next = symtab->root_sig;
+        symtab->root_sig = new_sig;
+
+        finalize_sig(new_sig);
+        result_sig = new_sig;
+    }
+
+    return result_sig;
 }

src/lily_emitter.c
@@ -482,6 +482,65 @@ static int template_check(lily_emit_state *emit, lily_sig *lhs, lily_sig *rhs)
     return ret;
 }
 
+/*  recursively_build_sig
+    This function is called when the return of a function uses templates.
+    It will return a signature that has the current template types replaced.
+
+    One example is the 'some' function, defined as 'function(A => maybe[A])'
+    some(10) # A = integer, so the result is maybe[integer]
+
+    This should only be called by build_templated_sig, because it needs some
+    adjustments to emitter's sig_stack_pos before and after it's called.
+
+    emit:
+    template_index: For templates, the offset where templates should grab
+                    signatures from. This is important because the emitter's
+                    sig stack is also used to store sigs in place.
+    sig:            The signature to examine. */
+static lily_sig *recursively_build_sig(lily_emit_state *emit, int template_index,
+        lily_sig *sig)
+{
+    lily_sig *ret = sig;
+
+    if (sig->siglist != NULL) {
+        int i, save_start;
+        lily_sig **siglist = sig->siglist;
+        if (emit->sig_stack_pos + sig->siglist_size > emit->sig_stack_size)
+            grow_sig_stack(emit);
+
+        save_start = emit->sig_stack_pos;
+
+        for (i = 0;i < sig->siglist_size;i++) {
+            lily_sig *inner_sig = recursively_build_sig(emit, template_index, siglist[i]);
+            emit->sig_stack[emit->sig_stack_pos] = inner_sig;
+            emit->sig_stack_pos++;
+        }
+
+        ret = lily_build_ensure_sig(emit->symtab, sig->cls, i,
+                emit->sig_stack, save_start);
+
+        emit->sig_stack_pos -= i;
+    }
+    else if (sig->cls->id == SYM_CLASS_TEMPLATE)
+        ret = emit->sig_stack[template_index + sig->template_pos];
+
+    return ret;
+}
+
+/*  build_untemplated_sig
+    This takes a given sig that has templates inside and returns a sig that has
+    those templates replaced. */
+static lily_sig *build_untemplated_sig(lily_emit_state *emit, lily_sig *sig)
+{
+    int save_template_index = emit->sig_stack_pos;
+
+    emit->sig_stack_pos += sig->template_pos;
+    lily_sig *ret = recursively_build_sig(emit, save_template_index, sig);
+    emit->sig_stack_pos -= sig->template_pos;
+
+    return ret;
+}
+
 /** Error helpers **/
 static void bad_arg_error(lily_emit_state *emit, lily_ast *ast,
     lily_sig *got, lily_sig *expected, int arg_num)
@@ -1942,6 +2001,8 @@ static void eval_call(lily_emit_state *emit, lily_ast *ast)
             /* Now grab out the wanted template position. */
             return_sig = first_arg_result->siglist[return_sig->template_pos];
         }
+        else if (return_sig->template_pos != 0)
+            return_sig = build_untemplated_sig(emit, return_sig);
 
         lily_storage *storage = get_storage(emit, return_sig, ast->line_num);
 

src/lily_parser.c
@@ -297,13 +297,12 @@ static lily_sig *collect_var_sig(lily_parse_state *parser, int flags)
         if (flags & CV_MAKE_VARS)
             get_named_var(parser, cls->sig, 0);
     }
-    else if (cls->id == SYM_CLASS_TUPLE) {
+    else if (cls->id == SYM_CLASS_TUPLE ||
+             cls->id == SYM_CLASS_LIST ||
+             cls->id == SYM_CLASS_HASH) {
         int i, save_pos;
 
         save_pos = parser->sig_stack_pos;
-        lily_sig *new_sig = lily_try_sig_for_class(parser->symtab, cls);
-        if (new_sig == NULL)
-            lily_raise_nomem(parser->raiser);
 
         NEED_NEXT_TOK(tk_left_bracket)
         lily_lexer(lex);
@@ -323,64 +322,27 @@ static lily_sig *collect_var_sig(lily_parse_state *parser, int flags)
                 break;
         }
 
-        lily_sig **siglist = lily_malloc(i * sizeof(lily_sig *));
-        if (siglist == NULL)
-            lily_raise_nomem(parser->raiser);
-
-        new_sig->siglist = siglist;
-        new_sig->siglist_size = i;
-        int j;
-        for (j = 0;j < i;j++)
-            siglist[j] = parser->sig_stack[save_pos + j];
-
-        parser->sig_stack_pos = save_pos;
-        result = lily_ensure_unique_sig(parser->symtab, new_sig);
-        if (flags & CV_MAKE_VARS)
-            get_named_var(parser, result, 0);
-    }
-    else if (cls->id == SYM_CLASS_LIST || cls->id == SYM_CLASS_HASH) {
-        int i;
-        lily_sig *new_sig = lily_try_sig_for_class(parser->symtab, cls);
-        if (new_sig == NULL)
-            lily_raise_nomem(parser->raiser);
-
-        lily_sig **siglist;
-        siglist = lily_malloc(cls->template_count * sizeof(lily_sig));
-        if (siglist == NULL)
-            lily_raise_nomem(parser->raiser);
-
-        new_sig->siglist = siglist;
-        new_sig->siglist_size = 0;
-
-        NEED_NEXT_TOK(tk_left_bracket)
-        lily_lexer(lex);
-
-        for (i = 0;i < cls->template_count;i++) {
-            lily_sig *inner_sig = collect_var_sig(parser, 0);
-            siglist[i] = inner_sig;
-            if (i != (cls->template_count - 1)) {
-                lily_lexer(lex);
-                NEED_CURRENT_TOK(tk_comma)
-                lily_lexer(lex);
-            }
+        if (parser->sig_stack_pos - save_pos != cls->template_count &&
+            cls->template_count != -1) {
+            lily_raise(parser->raiser, lily_ErrSyntax,
+                    "Class %s expects %d type(s), but got %d type(s).\n",
+                    cls->name, cls->template_count,
+                    parser->sig_stack_pos - save_pos);
         }
-        NEED_NEXT_TOK(tk_right_bracket)
-
-        new_sig->siglist_size = cls->template_count;
-        new_sig = lily_ensure_unique_sig(parser->symtab, new_sig);
 
         if (cls->id == SYM_CLASS_HASH) {
-            /* Don't use siglist for this check, because lily_ensure_unique_sig
-               could have destroyed it. Instead, do new_sig->siglist.
-               Classes that are valid hash keys are flagged as such, so check
-               for that flag. */
-            if ((new_sig->siglist[0]->cls->flags & CLS_VALID_HASH_KEY) == 0) {
+            /* For hash, make sure this type is a valid key. */
+            lily_sig *check_sig = parser->sig_stack[save_pos];
+            if ((check_sig->cls->flags & CLS_VALID_HASH_KEY) == 0) {
                 lily_raise(parser->raiser, lily_ErrSyntax,
-                        "'%T' is not a valid hash key.\n", new_sig->siglist[0]);
+                        "'%T' is not a valid hash key.\n", check_sig);
             }
         }
 
-        result = new_sig;
+        result = lily_build_ensure_sig(parser->symtab, cls, i,
+                parser->sig_stack, save_pos);
+
+        parser->sig_stack_pos = save_pos;
         if (flags & CV_MAKE_VARS)
             get_named_var(parser, result, 0);
     }

src/lily_symtab.h
@@ -74,4 +74,5 @@ lily_sig *lily_try_sig_for_class(lily_symtab *, lily_class *);
 void lily_hide_block_vars(lily_symtab *, lily_var *);
 lily_sig *lily_try_sig_from_ids(lily_symtab *, const int *);
 lily_sig *lily_ensure_unique_sig(lily_symtab *, lily_sig *);
+lily_sig *lily_build_ensure_sig(lily_symtab *, lily_class *, int, lily_sig **, int);
 #endif

commit d756343e0705653569f4017bf5a00d1eac9766c9

Fix invalid read caused by commit 963b7b9. Whoops.

src/lily_symtab.c
@@ -380,6 +380,9 @@ static int init_classes(lily_symtab *symtab)
                     sig->siglist = NULL;
                     sig->siglist_size = 0;
                     sig->flags = 0;
+                    /* Non-template signatures use this to mean that this sig
+                       does not have templates inside. */
+                    sig->template_pos = 0;
                     if (i == SYM_CLASS_ANY)
                         sig->flags |= SIG_MAYBE_CIRCULAR;
 