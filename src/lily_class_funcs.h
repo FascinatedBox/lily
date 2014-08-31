36e1903214d1fb4ab9028d29354b96376e57fd35
Fascinated Box <FascinatedBox@gmail.com>
2014-08-31 00:52:12 -0700

    Exceptions: == and != for exceptions work, plus a ::new for new exceptions.
    A new exception instance can now be created like this:
    
    DivisionByZeroError::new(message)
    
    This works for all errors that can be caught, aside from Exception since that
    is a base class. Additionally, comparison is now supported by instance values.
    
    No raise just yet though...

--- src/lily_class_funcs.h
+++ src/lily_class_funcs.h
@@ -11,6 +11,7 @@ int lily_list_eq(struct lily_vm_state_t *, int *, lily_value *, lily_value *);
 int lily_hash_eq(struct lily_vm_state_t *, int *, lily_value *, lily_value *);
 int lily_tuple_eq(struct lily_vm_state_t *, int *, lily_value *, lily_value *);
 int lily_generic_eq(struct lily_vm_state_t *, int *, lily_value *, lily_value *);
+int lily_instance_eq(struct lily_vm_state_t *, int *, lily_value *, lily_value *);
 
 void lily_gc_list_marker(int, lily_value *);
 void lily_gc_any_marker(int, lily_value *);
--- src/lily_cls_all_exceptions.h
+++ src/lily_cls_all_exceptions.h
@@ -2,5 +2,13 @@
 # define LILY_CLS_ALL_EXCEPTIONS_H
 
 extern int lily_nomemoryerror_setup(lily_class *);
+extern int lily_dbzerror_setup(lily_class *);
+extern int lily_indexerror_setup(lily_class *);
+extern int lily_badtcerror_setup(lily_class *);
+extern int lily_noreturnerror_setup(lily_class *);
+extern int lily_valueerror_setup(lily_class *);
+extern int lily_recursionerror_setup(lily_class *);
+extern int lily_keyerror_setup(lily_class *);
+extern int lily_formaterror_setup(lily_class *);
 
 #endif
