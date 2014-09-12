e165e0a25a2f89565a587814aa8bae652acf00f4
Jesse Ray Adkins <jesserayadkins@gmail.com>
2014-09-12 16:19:44 -0700

    Merge common binding in mod_lily and vm into lily_bind.{c,h}
    I should have done this when I was making exceptions, instead of copy+pasting
    big chunks of code from mod_lily.c. I also changed the functions that were
    put in common space to take a symtab (no more making a signature to pass it!)
    This means that each bind function has to do a symtab lookup, but that's okay.

---
+++ src/lily_bind.h
@@ -0,0 +1,12 @@
+#ifndef LILY_BIND_H
+# define LILY_BIND_H
+
+# include "lily_syminfo.h"
+# include "lily_symtab.h"
+
+lily_value *lily_bind_string(lily_symtab *, const char *);
+lily_value *lily_bind_integer(lily_symtab *, int64_t);
+lily_value *lily_bind_string_take_buffer(lily_symtab *, char *);
+void lily_bind_destroy(lily_value *);
+
+#endif
