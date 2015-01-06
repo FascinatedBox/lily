744cbb9c9f613932e0caea46dc6ae543584cbcc2
Fascinated Box <FascinatedBox@gmail.com>
2015-01-05 22:15:32 -0800

    Emitter: Document the fields of lily_emitter.h

--- src/lily_emitter.h
+++ src/lily_emitter.h
@@ -5,22 +5,48 @@
 # include "lily_symtab.h"
 
 typedef struct lily_block_ {
+    /* This block can use storages starting from here. */
+    lily_storage *storage_start;
+
+    /* Vars declared in this block are at var_start->next. */
+    lily_var *var_start;
+
+    /* Define/class blocks: This is the bytecode will be stored. */
+    lily_var *function_var;
+
+    /* Loop blocks: The position in bytecode where the loop starts.
+       For non-loop blocks, this is -1. */
     int16_t loop_start;
+
+    /* An index where the patches for this block start off. */
     uint16_t patch_start;
 
+    /* Define/class blocks: How many generics are available in this block. */
     uint16_t generic_count;
+
+    /* Match blocks: The starting position in emitter's match_cases. */
     uint16_t match_case_start;
 
     uint32_t block_type;
+
+    /* Match blocks: This is where the code for the match starts. Cases will
+       use this to write dispatching information. */
     uint32_t match_code_start;
+
+    /* Define/class blocks: Where the symtab's register allocation was before
+       entry. */
     uint32_t save_register_spot;
+
     uint32_t pad;
 
+    /* Match blocks: This sym holds a register spot and the type for helping
+       with match case checking. */
     lily_sym *match_sym;
-    lily_var *var_start;
-    lily_var *function_var;
-    lily_storage *storage_start;
+
+    /* The current class, or NULL if not within a class. */
     lily_class *class_entry;
+
+    /* Where 'self' is at, or NULL if not within a class. */
     lily_storage *self;
 
     struct lily_block_ *next;
@@ -31,41 +57,88 @@ typedef struct lily_block_ {
 struct lily_parse_state_t;
 
 typedef struct {
+    /* Patches are spots that the block needs to remember for when the block is
+       done. They're shared by all blocks, starting from the index of any
+       block.
+       One use of this is to make sure that the branches of an 'if' block all
+       get patched to the end of the block once the end is known. */
     int *patches;
+
+    /* Match blocks allocate space in here, initially with 0. When a match case
+       is seen, it's set to 1. This is used to make sure a case isn't seen
+       twice or not seen at all. */
     int *match_cases;
+
+    /* This stack is used primarily for holding unresolved types. Unresolved
+       types are in here from type_stack_pos + current_generic_adjust. All
+       positions above that are safe to use for any position. */
     lily_type **type_stack;
 
     uint16_t patch_pos;
+
     uint16_t patch_size;
 
     uint16_t match_case_pos;
+
     uint16_t match_case_size;
 
     uint16_t type_stack_pos;
+
     uint16_t type_stack_size;
 
+    /* Calls complete information about generics as they go along. Their
+       working information is stored in type_stack_pos + 0 to type_stack_pos +
+       this adjustment. This allows subtrees to grab generic information if
+       they want to. */
     uint32_t current_generic_adjust;
 
+    /* The var that will receive the function value when the function block is
+       done. */
     lily_var *top_var;
+
+    /* The value that holds the bytecode. */
     lily_function_val *top_function;
+
+    /* The return type of the current function. */
     lily_type *top_function_ret;
 
+    /* Either the last-entered class, or NULL. */
     lily_class *current_class;
 
+    /* The beginning of the linked list of storages, as well as the top. The
+       middle one, unused_storage_start, is used to make sure that functions
+       do not touch each other's (possibly in-use) storages. */
     lily_storage *all_storage_start;
     lily_storage *unused_storage_start;
     lily_storage *all_storage_top;
 
+    /* The currently entered block. This is never NULL, because __main__ is
+       implicitly entered before any user code. */
     lily_block *block;
 
+    /* How deep the current functions are. */
     uint32_t function_depth;
+
+    /* Each expression has a unique id so that the emitter knows not to reuse
+       a given storage within an expression. */
     uint32_t expr_num;
 
+    /* This is the current line number, at any given time. */
     uint16_t *lex_linenum;
 
     lily_raiser *raiser;
+
+    /* The ast pool stores names inside of here so that there's not allocations
+       to keep track of. This is typically used with x.y type evaluations which
+       are deferred to emit-time when type information is known. */
     lily_ast_str_pool *oo_name_pool;
+
+    /* The parser is stored within the emitter so that the emitter can do
+       dynamic loading of functions. */
     struct lily_parse_state_t *parser;
+
+    /* The symtab is here so the emitter can easily create storages if it needs
+       to, which is often. */
     lily_symtab *symtab;
 } lily_emit_state;
 
