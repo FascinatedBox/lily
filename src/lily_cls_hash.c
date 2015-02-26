b6890e29fd12d950743884c6284d64cd19babbbb
Jesse Ray Adkins <jesserayadkins@gmail.com>
2015-02-26 14:29:39 -0800

    Implement hash::keys (#33), and fix hash function oops.
    Parser now allows generic types to be the keys of a hash. This isn't
    the best solution though...because it forces the problem onto emitter
    (and possibly vm later) to make sure that hash keys are valid. The
    real, true solution is to, at some point in the future, implement
    traits and require the A of a hash to be hashable (which would mean it
    would have a hashing function).
    
    Also, this commit un-breaks hash functions. It turns out that not
    allowing the above causes hashes to raise an error with a silly
    message about A not being a valid hash key.

--- src/lily_cls_hash.c
+++ src/lily_cls_hash.c
@@ -1,6 +1,8 @@
 #include "lily_impl.h"
 #include "lily_vm.h"
 
+#define malloc_mem(size) vm->mem_func(NULL, size)
+
 void lily_hash_get(lily_vm_state *vm, lily_function_val *self, uint16_t *code)
 {
     lily_value **vm_regs = vm->vm_regs;
@@ -22,8 +24,57 @@ void lily_hash_get(lily_vm_state *vm, lily_function_val *self, uint16_t *code)
     lily_assign_value(vm, result, new_value);
 }
 
+/*  lily_hash_keys
+    Implements hash::keys
+
+    This function returns a list containing each key within the hash. */
+void lily_hash_keys(lily_vm_state *vm, lily_function_val *self, uint16_t *code)
+{
+    lily_value **vm_regs = vm->vm_regs;
+    lily_hash_val *hash_val = vm_regs[code[0]]->value.hash;
+    lily_value *result_reg = vm_regs[code[1]];
+
+    int num_elems = hash_val->num_elems;
+
+    lily_list_val *result_lv = malloc_mem(sizeof(lily_list_val));
+    result_lv->num_values = num_elems;
+    result_lv->visited = 0;
+    result_lv->refcount = 1;
+    result_lv->elems = malloc_mem(num_elems * sizeof(lily_value *));
+    result_lv->gc_entry = NULL;
+
+    lily_type *key_type = result_reg->type->subtypes[0];
+    int i = 0;
+
+    lily_hash_elem *elem_iter = hash_val->elem_chain;
+    while (elem_iter) {
+        lily_value *new_value = malloc_mem(sizeof(lily_value));
+        new_value->type = key_type;
+        new_value->value.integer = 0;
+        new_value->flags = VAL_IS_NIL;
+
+        lily_assign_value(vm, new_value, elem_iter->elem_key);
+        result_lv->elems[i] = new_value;
+
+        i++;
+        elem_iter = elem_iter->next;
+    }
+
+    /* This just has to check for nility, because lists are never marked as
+       protected. */
+    if ((result_reg->flags & VAL_IS_NIL) == 0)
+        lily_deref_list_val(vm->mem_func, result_reg->type,
+                result_reg->value.list);
+
+    result_reg->value.list = result_lv;
+    result_reg->flags = 0;
+}
+
+static const lily_func_seed keys =
+    {"keys", "function keys[A, B](hash[A, B] => list[A])", lily_hash_keys, NULL};
+
 static const lily_func_seed get =
-    {"get", "function get[A, B](hash[A, B], A, B => B)", lily_hash_get, NULL};
+    {"get", "function get[A, B](hash[A, B], A, B => B)", lily_hash_get, &keys};
 
 #define SEED_START get
 
