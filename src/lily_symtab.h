6b27d3394b11ccf9f48ebb12cfecc03e3b7c6ea7
Jesse Ray Adkins <jesserayadkins@gmail.com>
2015-04-03 17:11:26 -0700

    lily_find_import no longer takes an import to look in.
    By default, each lily_find_* will look through the active stuff, then
    the builtin stuff. If something wants to look through a specific
    import, then it should call 'lily_find_import_within' with a
    particular import.

--- src/lily_symtab.h
+++ src/lily_symtab.h
@@ -158,7 +158,7 @@ void lily_change_parent_class(lily_class *, lily_class *);
 
 void lily_enter_import(lily_symtab *, lily_import_entry *);
 void lily_leave_import(lily_symtab *);
-lily_import_entry *lily_find_import(lily_symtab *, lily_import_entry *, char *);
+lily_import_entry *lily_find_import(lily_symtab *, char *);
 lily_import_entry *lily_find_import_within(lily_import_entry *, char *);
 
 const lily_func_seed *lily_get_global_seed_chain();
