a2162f1d483a9e0c2cb5083b9ec5f94bcf40e268
Jesse Ray Adkins <jesserayadkins@gmail.com>
2015-04-14 17:32:57 -0700

    Rewrite pre commit for a tag mode directory. Fixes #109.
    The pre commit hook no longer cares about how many tests are run. It
    just runs them and prints a total at the end if there's no issue. I'm
    more interested in the fact that they passed than which one out of the
    total (numbered) failed.
    
    Additionally, I've touched up all of the tests in the passing
    directory so that they do stderr.write instead of print. By doing so,
    the pre-commit-hook can ignore anything dumped to stdout (which is
    where print and show() go).
    
    I've also added a tag_mode directory which is there to ensure that
    running the interpreter in tagged mode doesn't lead to a segfault.
    Since stdout is now ignored, the tagged mode stuff can output whatever
    stuff it likes.

--- src/pre-commit-hook.py
+++ src/pre-commit-hook.py
@@ -10,26 +10,10 @@
 
 import os, subprocess, sys, signal
 
-pass_entries = []
-fail_entries = []
-MODE_PASS = 1
-MODE_FAIL = 0
-
 pass_count = 0
 error_count = 0
 crash_count = 0
-
-def load_filenames(path):
-    ret = {}
-    for (dirpath, dirnames, filenames) in os.walk(path):
-        for name in filenames:
-            path = dirpath + '/'
-            try:
-                ret[path].append(name)
-            except:
-                ret[path] = [name]
-
-    return ret
+test_count = 0
 
 def get_expected_str(filename):
     f = open(filename, "r")
@@ -44,28 +28,20 @@ def get_expected_str(filename):
             output += line.rstrip("\r\n") + "\n"
     return output
 
-def basic_run(filename, basedir, mode, run_range):
-    global pass_count, error_count, crash_count
-    should_pass = (mode == MODE_PASS)
-    mode_str = ""
-    if mode == MODE_PASS:
-        mode_str = "pass"
-    else:
-        mode_str = "fail"
+def run_test(options, dirpath, filepath):
+    global pass_count, error_count, crash_count, test_count
 
-    path = basedir + filename
-    # The -g 2 is so that Lily's garbage collector will only allow 2
-    # objects at the same time before doing a gc sweep.
-    # This ensures that tests aren't passing only because the gc is not
-    # being triggered.
-    command = "./lily -g 2 %s" % (path)
+    test_count += 1
+
+    should_pass = (options['mode'] == 'pass')
+    fullpath = dirpath + filepath
+    expected_stderr = get_expected_str(fullpath)
+
+    command = "./lily %s %s" % (options['invoke'], fullpath)
     subp = subprocess.Popen([command], stdout=subprocess.PIPE,
             stderr=subprocess.PIPE, shell=True)
     (subp_stdout, subp_stderr) = subp.communicate()
     subp.wait()
-
-    did_pass = True
-    expected_stderr = get_expected_str(path)
 
     try:
         if subp_stderr[-1] == '\n':
@@ -75,11 +51,10 @@ def basic_run(filename, basedir, mode, run_range):
 
     # This makes it so that different tests don't have to reference the
     # directory path in the error (which is tedious and annoying).
-    subp_stderr = subp_stderr.replace(basedir, "");
+    subp_stderr = subp_stderr.replace(dirpath, "");
 
-    if  len(subp_stdout) or \
-        subp_stderr != expected_stderr or \
-        subp.returncode == -signal.SIGSEGV:
+    if subp_stderr != expected_stderr or \
+       subp.returncode == -signal.SIGSEGV:
 
         message = ""
         if subp.returncode == -signal.SIGSEGV:
@@ -89,43 +64,43 @@ def basic_run(filename, basedir, mode, run_range):
             message = "!!!FAILED!!!"
             error_count += 1
 
-        sys.stdout.write("[%3d/%3d] test %s %s\n" % (run_range[0], \
-                run_range[1], os.path.basename(filename), message))
+        sys.stdout.write("#%d test %s %s\n" % (test_count, \
+                os.path.basename(filepath), message))
     else:
         pass_count += 1
 
-def count_values(entries):
-    result = 0
+def get_options_for(dirpath):
+    options = {'invoke': '-g 2'}
 
-    for k in entries.iterkeys():
-        result += len(entries[k])
+    # By default, the gc threshold is set to 2 so that the tests don't
+    # pass only because the garbage collector isn't invoked.
+    if dirpath.endswith('tag_mode/'):
+        # This causes the tests run in this directory to be run in
+        # tagged mode (code will be between <?lily ... ?> tags only).
+        options['invoke'] += ' -t'
 
-    return result
+    options['mode'] = 'pass' if dirpath.startswith('test/pass') else 'fail'
+    return options
 
-pass_entries = load_filenames('test/pass')
-pass_entry_count = count_values(pass_entries)
-fail_entries = load_filenames('test/fail')
-fail_entry_count = count_values(fail_entries)
-run_range = [0, pass_entry_count + fail_entry_count]
+def process_test_dir(basepath):
+    mode = 'pass' if basepath == 'test/pass' else 'fail'
 
-print "pre-commit-hook.py: Running %d passing tests..." % pass_entry_count
-for dirpath in pass_entries:
-    for filename in pass_entries[dirpath]:
-        run_range[0] += 1
-        basic_run(filename, dirpath, MODE_PASS, run_range)
+    # I use _list as a suffix to make the difference more obvious than,
+    # say, filepaths versus filepath.
+    for (dirpath, dirpath_list, filepath_list) in os.walk(basepath):
+        dirpath += '/'
+        options = get_options_for(dirpath)
+        for filepath in filepath_list:
+            run_test(options, dirpath, filepath)
 
-print "pre-commit-hook.py: Running %d failing tests..." % fail_entry_count
-for dirpath in fail_entries:
-    for filename in fail_entries[dirpath]:
-        run_range[0] += 1
-        basic_run(filename, dirpath, MODE_FAIL, run_range)
+if __name__ == '__main__':
+    process_test_dir('test/fail')
+    process_test_dir('test/pass')
 
-print ("Final stats: %d tests passed, %d errors, %d crashed." \
+print ('Final stats: %d tests passed, %d errors, %d crashed.' \
         % (pass_count, error_count, crash_count))
 
 if error_count == 0 and crash_count == 0:
     sys.exit(0)
 else:
     sys.exit(1)
-
-
