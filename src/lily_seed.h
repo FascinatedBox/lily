da34d2527fa064ff589931220ae967f728bc727b
Jesse Ray Adkins <jesserayadkins@gmail.com>
2015-05-12 14:43:32 -0700

    I can't believe I forgot to check this in. >.>

---
+++ src/lily_seed.h
@@ -0,0 +1,36 @@
+#ifndef LILY_SEED_H
+# define LILY_SEED_H
+
+typedef enum {
+    dyna_class,
+    dyna_function
+} dyna_type;
+
+typedef const struct {
+    const void *next;
+    char *name;
+    uint64_t seed_type;
+} lily_base_seed;
+
+typedef const struct {
+    const void *next;
+    char *name;
+    uint64_t seed_type;
+    char *func_definition;
+    lily_foreign_func func;
+} lily_func_seed;
+
+typedef const struct {
+    const void *next;
+    char *name;
+    uint64_t seed_type;
+    uint16_t is_refcounted;
+    uint16_t generic_count;
+    uint32_t flags;
+    class_setup_func setup_func;
+    gc_marker_func gc_marker;
+    class_eq_func eq_func;
+    class_destroy_func destroy_func;
+} lily_class_seed;
+
+#endif
