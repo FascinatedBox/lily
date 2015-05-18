b3db5ff202475d132603ceec9a0d0785b3a4e5a8
Fascinated Box <FascinatedBox@gmail.com>
2015-05-18 14:48:51 -0700

    Move error message creation into parser.

--- run/lily.c
+++ run/lily.c
@@ -72,72 +72,6 @@ static void process_args(int argc, char **argv)
     }
 }
 
-void traceback_to_file(lily_parse_state *parser, FILE *outfile)
-{
-    lily_raiser *raiser = parser->raiser;
-    if (raiser->exception) {
-        /* If this error is not one of the builtin ones, then show the package
-           from where it came. The reason for this is that different packages
-           may wish to export a general error class (ex: pg::Error,
-           mysql::Error, etc). So making it clear -which- one can be useful. */
-        char *loadname = raiser->exception->type->cls->import->loadname;
-        if (strcmp(loadname, "") != 0)
-            fprintf(outfile, "%s::", loadname);
-    }
-    fprintf(outfile, "%s", lily_name_for_error(raiser));
-    if (raiser->msgbuf->message[0] != '\0')
-        fprintf(outfile, ": %s", raiser->msgbuf->message);
-    else
-        fputc('\n', outfile);
-
-    if (parser->executing == 0) {
-        lily_lex_entry *iter = parser->lex->entry;
-
-        int fixed_line_num = (raiser->line_adjust == 0 ?
-                parser->lex->line_num : raiser->line_adjust);
-
-        /* The parser handles lambda processing by putting entries with the
-           name [lambda]. Don't show these. */
-        while (strcmp(iter->filename, "[lambda]") == 0)
-            iter = iter->prev;
-
-        /* Since importing is not yet possible, simply show the top entry. This
-           should be the actual file loaded. */
-        iter->saved_line_num = fixed_line_num;
-        fprintf(outfile, "Where: File \"%s\" at line %d\n", iter->filename,
-                iter->saved_line_num);
-    }
-    else {
-        lily_vm_stack_entry **vm_stack;
-        lily_vm_stack_entry *entry;
-        int i;
-
-        vm_stack = parser->vm->function_stack;
-        fprintf(outfile, "Traceback:\n");
-
-        for (i = parser->vm->function_stack_pos-1;i >= 0;i--) {
-            entry = vm_stack[i];
-            char *class_name = entry->function->class_name;
-            char *separator;
-            if (class_name == NULL) {
-                class_name = "";
-                separator = "";
-            }
-            else
-                separator = "::";
-
-            if (entry->function->code == NULL)
-                fprintf(outfile, "    File \"%s\", from %s%s%s\n",
-                        entry->function->path, class_name, separator,
-                        entry->function->trace_name);
-            else
-                fprintf(outfile, "    File \"%s\", from %s%s%s at line %d\n",
-                        entry->function->path, class_name, separator,
-                        entry->function->trace_name, entry->line_num);
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
     process_args(argc, argv);
@@ -161,7 +95,7 @@ int main(int argc, char **argv)
         result = lily_parse_string(parser, "[cli]", mode, to_process);
 
     if (result == 0) {
-        traceback_to_file(parser, stderr);
+        fputs(lily_build_error_message(parser), stderr);
         exit(EXIT_FAILURE);
     }
 
