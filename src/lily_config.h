f9e54c279629f306409532c02b7369d6e5e6356d
Fascinated Box <FascinatedBox@gmail.com>
2015-05-20 12:42:00 -0700

    Fix up the embarassing mess of path seeding. Yikes. :(

--- src/lily_config.h
+++ src/lily_config.h
@@ -3,17 +3,18 @@
    The biggest one is the lack of Windows support, because I don't have
    my Windows box setup for Lily yet (I'd rather make sure Windows works
    all at once). */
-#define LILY_MAJOR "0"
-#define LILY_MINOR "13"
-#define LILY_VERSION_DIR   LILY_MAJOR "." LILY_MINOR
+#define LILY_MAJOR          "0"
+#define LILY_MINOR          "13"
+#define LILY_VERSION_DIR    LILY_MAJOR "_" LILY_MINOR
 
-#define LILY_BASE_DIR	"/usr/local/"
-#define LILY_SHARE_DIR	LILY_ROOT "share/lua/" LUA_VDIR "/"
-#define LILY_CDIR	LILY_ROOT "lib/lua/" LUA_VDIR "/"
+#define LILY_BASE_DIR       "/usr/local/"
+#define LILY_SHARE_DIR      LILY_BASE_DIR "share/lily/" LILY_VERSION_DIR "/"
+#define LILY_LIB_DIR        LILY_BASE_DIR "lib/lily/"   LILY_VERSION_DIR "/"
 
-/* So, by default, Lily will attempt to load .lly files from these
-   directories. */
+/* This is where Lily will attempt to import new files from. If the parser is
+   loading from a file, then the directory of the first file is added as the
+   very last path to search. */
 #define LILY_PATH_SEED \
-        LILY_BASE_DIR "share/" LILY_VERSION_DIR "/;" \
-        LILY_BASE_DIR "lib/" LILY_VERSION_DIR "/;" \
+        LILY_SHARE_DIR ";" \
+        LILY_LIB_DIR ";" \
         "./;"
