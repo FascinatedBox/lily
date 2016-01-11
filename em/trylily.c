0d7f4824318756ff08682def0088177229dd607c
Jesse Ray Adkins <jesserayadkins@gmail.com>
2016-01-11 22:49:57 +0000

    Build with emscripten...sort of
    This is incomplete, but a step toward a 'try it online' page. This only
    does the .js build right now though.

---
+++ em/trylily.c
@@ -0,0 +1,35 @@
+#include "lily_parser.h"
+
+/* This file is to be compiled only by emscripten, and serves as a bridge
+   between the js of a browser and Lily's C code.
+   Since emscripten can't run main() twice, this instead supplies a series of
+   helper functions to be called by the js side. It also has the impl for puts
+   that's necessary. */
+
+void lily_impl_puts(void *data, char *text)
+{
+    fputs(text, stdout);
+}
+
+lily_parse_state *get_parser()
+{
+    lily_options *options = lily_new_default_options();
+    lily_parse_state *parser = lily_new_parse_state(options);
+
+    return parser;
+}
+
+int run_parser(lily_parse_state *parser, char *to_parse)
+{
+    return lily_parse_string(parser, "[tryit]", lm_no_tags, to_parse);
+}
+
+void destroy_parser(lily_parse_state *parser)
+{
+    lily_free_parse_state(parser);
+}
+
+char *get_parser_error(lily_parse_state *parser)
+{
+    return lily_build_error_message(parser);
+}
