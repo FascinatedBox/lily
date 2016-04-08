677e0ed34e74116716fc795cf25e7c7b91a49198
Jesse Ray Adkins <jesserayadkins@gmail.com>
2016-04-08 13:45:52 -0700

    Organize lily_vm_state's layout, remove another unused field.

--- src/lily_vm.h
+++ src/lily_vm.h
@@ -59,30 +59,32 @@ typedef struct {
 typedef struct lily_vm_state_ {
     lily_value **vm_regs;
     lily_value **regs_from_main;
+
+    /* The total number of registers available, minus 2. This is used to check
+       for space checks so that there are always 2 registers available for
+       foreign calls without needing to check. */
+    uint32_t offset_max_registers;
+
+    /* The number of registers currently being used. */
+    uint32_t num_registers;
+
+    /* The actual total, for parts like gc collection + register grow. */
+    uint32_t true_max_registers;
+
+    uint32_t call_depth;
+
     lily_call_frame *call_chain;
 
     lily_tie **readonly_table;
     lily_class **class_table;
-    lily_type_buffer *cast_types;
-
-    /* A linked list of entries that are currently being used. */
-    lily_gc_entry *gc_live_entries;
-    /* A linked list of entries not currently used. Entries which have their
-       values swept are put here. This is checked before making a new node. */
-
-    lily_gc_entry *gc_spare_entries;
-
-    uint32_t num_registers;
-
-    /* This is how many registers the vm has made, minus 2. This is used along
-       with the number of registers to determine */
-    uint32_t offset_max_registers;
-    uint32_t true_max_registers;
-
     uint32_t class_count;
     uint32_t readonly_count;
 
-    uint32_t call_depth;
+    /* A linked list of entries that should be findable from a register. */
+    lily_gc_entry *gc_live_entries;
+
+    /* A linked list of entries not currently in use. */
+    lily_gc_entry *gc_spare_entries;
 
     /* How many entries are in ->gc_live_entries. If this is >= ->gc_threshold,
        then the gc is triggered when there is an attempt to attach a gc_entry
@@ -97,12 +99,10 @@ typedef struct lily_vm_state_ {
 
     uint64_t prep_id_start;
 
-    /* Most of the stack entries will be native functions, with the lowest
-       being __main__. __main__ has o_return_from_vm at the end, so native
-       functions are fine.
-       Foreign functions do not have native code, and they need to bail out of
-       the vm. So the vm lies and says this "foreign code" as its code. This
-       contains a o_return_from_vm as its instruction. */
+    /* Foreign functions will put this in the code portion of their stack so
+       that lily_vm_execute will bail out instead of a normal return. This is
+       stored in the vm so that foreign call prep only has to set it on the
+       call frame once. */
     uint16_t *foreign_code;
 
     char *sipkey;
@@ -110,17 +110,19 @@ typedef struct lily_vm_state_ {
     lily_vm_catch_entry *catch_top;
     lily_vm_catch_entry *catch_chain;
 
-    struct lily_parse_state_ *parser;
-
+    /* This buffer is used as an intermediate storage for String values. */
     lily_msgbuf *vm_buffer;
 
-    /* This is used as an intermediate storage, when the resulting size of a
-       list is not yet known. This prevents functions from doing repeated
-       reallocs (in the case of large result lists), or from overallocating (in
-       the case of small result lists). */
+    /* This is used as an intermediate storage for List values. */
     lily_vm_list *vm_list;
+
+    /* This is used to dynaload exceptions when absolutely necessary. */
+    struct lily_parse_state_ *parser;
     lily_symtab *symtab;
     lily_raiser *raiser;
+    /* This holds the 'data' blob passed in to the interpreter's options. The
+       mod_lily module uses this to hold the request_rec so that server
+       functions can fetch it back out. */
     void *data;
 
     /* If stdout has been dynaloaded, then this is the register that holds
