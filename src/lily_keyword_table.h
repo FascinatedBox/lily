abcb38a3fa2e7550405673ca6b5937caae6421fb
Fascinated Box <FascinatedBox@gmail.com>
2016-05-23 15:42:30 -0700

    Create a constant table alongside the keyword table.
    I did this to centralize magic keyword handling to just expression's
    word handling.

--- src/lily_keyword_table.h
+++ src/lily_keyword_table.h
@@ -6,6 +6,23 @@ typedef struct {
     uint64_t shorthash;
 } keyword_entry;
 
+keyword_entry constants[] = {
+    {"true",         1702195828},
+    {"self",         1718379891},
+    {"false",        435728179558},
+    {"__file__",     6872323072689856351},
+    {"__line__",     6872323081280184159},
+    {"__function__", 7598807797348065119},
+};
+
+#define CONST_TRUE        0
+#define CONST_SELF        1
+#define CONST_FALSE       2
+#define CONST__FILE__     3
+#define CONST__LINE__     4
+#define CONST__FUNCTION__ 5
+#define CONST_LAST_ID     5
+
 keyword_entry keywords[] = {
     {"if",           26217},
     {"do",           28516},
@@ -15,13 +32,10 @@ keyword_entry keywords[] = {
     {"try",          7959156},
     {"case",         1702060387},
     {"else",         1702063205},
-    {"true",         1702195828},
     {"elif",         1718185061},
-    {"self",         1718379891},
     {"enum",         1836412517},
     {"while",        435610544247},
     {"raise",        435727982962},
-    {"false",        435728179558},
     {"match",        448345170285},
     {"break",        461195539042},
     {"class",        495857003619},
@@ -30,11 +44,8 @@ keyword_entry keywords[] = {
     {"except",       128026086176869},
     {"import",       128034844732777},
     {"private",      28556934595048048},
-    {"__file__",     6872323072689856351},
-    {"__line__",     6872323081280184159},
     {"protected",    7310577382525465200},
     {"continue",     7310870969309884259},
-    {"__function__", 7598807797348065119},
 };
 
 # define KEY_IF            0
@@ -45,26 +56,20 @@ keyword_entry keywords[] = {
 # define KEY_TRY           5
 # define KEY_CASE          6
 # define KEY_ELSE          7
-# define KEY_TRUE          8
-# define KEY_ELIF          9
-# define KEY_SELF         10
-# define KEY_ENUM         11
-# define KEY_WHILE        12
-# define KEY_RAISE        13
-# define KEY_FALSE        14
-# define KEY_MATCH        15
-# define KEY_BREAK        16
-# define KEY_CLASS        17
-# define KEY_DEFINE       18
-# define KEY_RETURN       19
-# define KEY_EXCEPT       20
-# define KEY_IMPORT       21
-# define KEY_PRIVATE      22
-# define KEY__FILE__      23
-# define KEY__LINE__      24
-# define KEY_PROTECTED    25
-# define KEY_CONTINUE     26
-# define KEY__FUNCTION__  27
-# define KEY_LAST_ID      27
+# define KEY_ELIF          8
+# define KEY_ENUM          9
+# define KEY_WHILE        10
+# define KEY_RAISE        11
+# define KEY_MATCH        12
+# define KEY_BREAK        13
+# define KEY_CLASS        14
+# define KEY_DEFINE       15
+# define KEY_RETURN       16
+# define KEY_EXCEPT       17
+# define KEY_IMPORT       18
+# define KEY_PRIVATE      19
+# define KEY_PROTECTED    20
+# define KEY_CONTINUE     21
+# define KEY_LAST_ID      21
 
 #endif
