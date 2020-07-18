2fa54a6076d866190c1d94e48b3ef850a9dd0cbe
FascinatedBox <FascinatedBox@protonmail.com>
2020-07-17 22:53:52 -0700

    config: Add missing include guard.

--- src/lily_config.h
+++ src/lily_config.h
@@ -1,9 +1,13 @@
-#ifdef _WIN32
-# define LILY_PATH_CHAR '\\'
-# define LILY_PATH_SLASH "\\"
-# define LILY_LIB_SUFFIX "dll"
-#else
-# define LILY_PATH_CHAR '/'
-# define LILY_PATH_SLASH "/"
-# define LILY_LIB_SUFFIX "so"
+#ifndef LILY_CONFIG_H
+# define LILY_CONFIG_H
+
+# ifdef _WIN32
+#  define LILY_PATH_CHAR '\\'
+#  define LILY_PATH_SLASH "\\"
+#  define LILY_LIB_SUFFIX "dll"
+# else
+#  define LILY_PATH_CHAR '/'
+#  define LILY_PATH_SLASH "/"
+#  define LILY_LIB_SUFFIX "so"
+# endif
 #endif
