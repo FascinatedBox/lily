72ad04d3ba5ddcdc0523942c7a860de27ce0cf70
FascinatedBox <FascinatedBox@protonmail.com>
2021-05-17 15:36:31 -0700

    Add keyword argument to `String.find` (fixes #519).

--- src/lily_pkg_prelude_bindings.h
+++ src/lily_pkg_prelude_bindings.h
@@ -90,7 +90,7 @@ const char *lily_prelude_info_table[] = {
     ,"m\0<new>\0(String): RuntimeError"
     ,"C\025String\0"
     ,"m\0ends_with\0(String,String): Boolean"
-    ,"m\0find\0(String,String,*Integer): Option[Integer]"
+    ,"m\0find\0(String,String,:start *Integer): Option[Integer]"
     ,"m\0format\0(String,$1...): String"
     ,"m\0html_encode\0(String): String"
     ,"m\0is_alnum\0(String): Boolean"
